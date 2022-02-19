d39bf8b4e24cf1a8a70a6c0025360e3dc38c6ffb
oucouc oussama <oussama.ouq@gmail.com>
2022-02-19 17:45:44 +0100

    adding out compnent

---
+++ src/view/App.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+import '../styles/dashboard.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import React from 'react';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
